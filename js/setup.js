bf6616d4e74274ff5537cb8a5a6aebd1237cc788
Kasinerava <65457233+Kasinerava@users.noreply.github.com>
2020-09-29 01:33:45 +0300

    Последние правки волшебников

--- js/setup.js
+++ js/setup.js
@@ -10,8 +10,8 @@ const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
   .querySelector(`.setup-similar-item`);
 
 const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const WIZARD_SURNAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const COATS_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
 
 function getRandomInteger(min, max) {
@@ -19,15 +19,15 @@ function getRandomInteger(min, max) {
 }
 
 function getRandomArrayElement(array) {
-  return array[getRandomInteger(array.length, array.length - 1)];
+  return array[getRandomInteger(0, array.length - 1)];
 }
 
-const fragment = document.createFragment();
+const fragment = document.createDocumentFragment();
 
 for (let i = 0; i < 4; i++) {
   const wizardElement = similarWizardTemplate.cloneNode(true);
-  const nameWizard = `${getRandomArrayElement(WIZARD_NAMES)} ${getRandomArrayElement(WIZARD_SURNAME)}`;
-  const coatColor = getRandomArrayElement(COAT_COLOR);
+  const nameWizard = `${getRandomArrayElement(WIZARD_NAMES)} ${getRandomArrayElement(WIZARD_SURNAMES)}`;
+  const coatColor = getRandomArrayElement(COATS_COLOR);
   const eyesColor = getRandomArrayElement(EYES_COLOR);
 
   wizardElement.querySelector(`.setup-similar-label`).textContent = nameWizard;
