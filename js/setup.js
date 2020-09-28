768b9c78effd1cc2885942d76b7651d259952175
Kasinerava <65457233+Kasinerava@users.noreply.github.com>
2020-09-29 01:20:16 +0300

    Правки волшебников

--- js/setup.js
+++ js/setup.js
@@ -10,20 +10,30 @@ const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
   .querySelector(`.setup-similar-item`);
 
 const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const WIZARD_SURNAME = [` да Марья`, ` Верон`, ` Мирабелла`, ` Вальц`, ` Онопко`, ` Топольницкая`, ` Нионго`, ` Ирвинг`];
+const WIZARD_SURNAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
 
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomArrayElement(array) {
+  return array[getRandomInteger(array.length, array.length - 1)];
+}
+
+const fragment = document.createFragment();
 
 for (let i = 0; i < 4; i++) {
-  let wizardElement = similarWizardTemplate.cloneNode(true);
-  const nameWizard = WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + WIZARD_SURNAME[[Math.floor(Math.random() * WIZARD_SURNAME.length)]];
-  const coatColor = COAT_COLOR[Math.floor(Math.random() * COAT_COLOR.length)];
-  const eyesColor = EYES_COLOR[Math.floor(Math.random() * EYES_COLOR.length)];
+  const wizardElement = similarWizardTemplate.cloneNode(true);
+  const nameWizard = `${getRandomArrayElement(WIZARD_NAMES)} ${getRandomArrayElement(WIZARD_SURNAME)}`;
+  const coatColor = getRandomArrayElement(COAT_COLOR);
+  const eyesColor = getRandomArrayElement(EYES_COLOR);
 
   wizardElement.querySelector(`.setup-similar-label`).textContent = nameWizard;
   wizardElement.querySelector(`.wizard-coat`).style.fill = coatColor;
   wizardElement.querySelector(`.wizard-eyes`).style.fill = eyesColor;
 
-  similarListElement.appendChild(wizardElement);
+  fragment.appendChild(wizardElement);
 }
+similarListElement.appendChild(fragment);
