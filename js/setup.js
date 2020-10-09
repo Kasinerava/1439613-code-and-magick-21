9c362ae2544dbaa458bd0284dd9788f74573ffde
Kasinerava <65457233+Kasinerava@users.noreply.github.com>
2020-10-09 21:19:52 +0300

    Одежда глаза и форма

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,83 @@
 'use strict';
 
-const userDialog = document.querySelector(`.setup`);
-userDialog.classList.remove(`hidden`);
+const userSettings = document.querySelector(`.setup`);
+const setupOpen = document.querySelector(`.setup-open`);
+const setupClose = userSettings.querySelector(`.setup-close`);
+
+const onPopupEscPress = function (evt) {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const openPopup = function () {
+  userSettings.classList.remove(`hidden`);
+  document.addEventListener(`keydown`, onPopupEscPress);
+};
+
+const closePopup = function () {
+  userSettings.classList.add(`hidden`);
+  document.removeEventListener(`keydown`, onPopupEscPress);
+};
+
+setupOpen.addEventListener(`click`, function () {
+  openPopup();
+});
+
+setupOpen.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener(`click`, function () {
+  closePopup();
+});
+
+setupClose.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    closePopup();
+  }
+});
+
+const MIN_NAME_LENGTH = 2;
+const MAX_NAME_LENGTH = 25;
+
+const userNameInput = document.querySelector(`.setup-user-name`);
+
+userNameInput.addEventListener(`input`, function () {
+  const valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity(`Ещё ` + (MIN_NAME_LENGTH - valueLength) + ` симв.`);
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity(`Удалите лишние ` + (valueLength - MAX_NAME_LENGTH) + ` симв.`);
+  } else {
+    userNameInput.setCustomValidity(``);
+  }
+
+  userNameInput.reportValidity();
+});
+
+// Изменение цвета одежды глаз и фаербола
+
+const setupWizard = document.querySelector(`.setup-wizard`);
+const wizardCoat = setupWizard.querySelector(`.wizard-coat`);
+const wizardEyes = setupWizard.querySelector(`.wizard-eyes`);
+const wizardFireball = document.querySelector(`.setup-fireball-wrap`);
+
+wizardCoat.addEventListener(`click`, function () {
+  wizardCoat.style.fill = getRandomArrayElement(COATS_COLOR);
+});
+
+wizardEyes.addEventListener(`click`, function () {
+  wizardEyes.style.fill = getRandomArrayElement(EYES_COLOR);
+});
+
+wizardFireball.addEventListener(`click`, function () {
+  wizardFireball.style.backgroundColor = getRandomArrayElement(FIREBALL_COLOR);
+});
 
 document.querySelector(`.setup-similar`).classList.remove(`hidden`);
 const similarListElement = document.querySelector(`.setup-similar-list`);
@@ -13,6 +89,7 @@ const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `
 const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COATS_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
+const FIREBALL_COLOR = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
 
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
